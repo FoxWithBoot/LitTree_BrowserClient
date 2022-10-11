8ec94eedf9d86cc83387faceef8b86ab9661cd8c
FoxWithBoot <mr.mixan01@mail.tu>
2022-10-11 12:17:47 +0300

    model

--- js/sidebar.js
+++
@@ -1,6 +0,0 @@
-// Скрипт для работы с левым сайдбаром
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
-});
